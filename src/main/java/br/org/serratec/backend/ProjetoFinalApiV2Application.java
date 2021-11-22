aed811dfe3ceffae2190e9b18ac7abf57e8ec9e1
Délio Marcos <deliomarcos.contato@gmail.com>
2021-11-21 22:35:39 -0300

    Commit inicial

---
+++ src/main/java/br/org/serratec/backend/ProjetoFinalApiV2Application.java
@@ -0,0 +1,13 @@
+package br.org.serratec.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalApiV2Application.class, args);
+	}
+
+}
